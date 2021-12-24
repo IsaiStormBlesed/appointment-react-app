f9e98752f141fab25b313c52f8da43e93e856d57
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2021-12-24 18:30:54 +0000

    Added final styles and components.

--- src/components/PatientList.jsx
+++ src/components/PatientList.jsx
@@ -1,7 +1,17 @@
+import Patient from "./Patient"
+
 const PatientList = () => {
     return (
-        <div className="md:basis-1/2 lg:basis-3/5">
-            Patients
+        <div className="md:basis-1/2 lg:basis-3/5 overflow-y-scroll md:h-screen">
+            <h2 className="font-black text-3xl text-center">List of Patients</h2>
+            <p className="text-center mt-5 text-lg">
+                See and Manage your
+                <span className="text-sky-600 font-bold"> Pets</span>
+            </p>
+
+            <Patient />
+            <Patient />
+            <Patient />
         </div>
     )
 }
