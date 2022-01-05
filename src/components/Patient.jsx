0763a10819bfd0de13e2040bcb810c925c31951c
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2022-01-05 16:06:00 +0000

    Add edit & delete buttons.

--- src/components/Patient.jsx
+++ src/components/Patient.jsx
@@ -2,7 +2,7 @@ const Patient = ({patient}) => {
     const {name, owner, email, date, sympthoms} = patient
     return (
         <div>
-            <div className="bg-white px-5 py-10 shadow-md rounded-md mt-10 ml-5">
+            <div className="bg-white px-5 py-7 shadow-md rounded-md mt-10 ml-5">
                 <p className="font-bold text-gray-700 uppercase mb-5">
                     Name:
                     <span className="font-normal normal-case"> {name}</span>
@@ -23,6 +23,18 @@ const Patient = ({patient}) => {
                     Sympthoms:
                     <span className="font-normal normal-case"> {sympthoms}</span>
                 </p>
+
+                <div className="flex gap-2">
+                    <button 
+                        className="bg-sky-600 text-white uppercase hover:bg-sky-700 rounded-md px-4 py-2 mt-3">
+                        Edit
+                    </button>
+
+                    <button 
+                        className="bg-red-600 text-white uppercase hover:bg-red-700 rounded-md px-4 py-2 mt-3">
+                        Delete
+                    </button>
+                </div>
             </div>
         </div>
     )
